f09f0781eb32c90e9c9f6b48f8ee16dcc08d9a9d
Dima <dimas.kg93@gmail.com>
2021-01-29 15:48:44 +0600

    add phone mask

--- js/settings-mask.js
+++ js/settings-mask.js
@@ -39,10 +39,10 @@ function maskPhoneField() {
 
 
 // <script src="js/jquery-2.2.4.js"></script> -- 1 подключить Jquery
-// <script src="intl-tel-input-custom/js/intlTelInput.js"></script> -- 2 подключить intlTelInput.js
-// <script src="intl-tel-input-custom/js/imask.js"></script> -- 3 подключить imask.js
-// <script src="intl-tel-input-custom/js/settings-mask.js"></script> -- 3 подключить иницилизацию settings-mask.js
-// <link rel="stylesheet" href="intl-tel-input-custom/css/intlTelInput.css"> -- 4 подключить стили intlTelInput.css
+// <script src="intl-tel-input-mask__custom/js/intlTelInput.js"></script> -- 2 подключить intlTelInput.js
+// <script src="intl-tel-input-mask__custom/js/imask.js"></script> -- 3 подключить imask.js
+// <script src="intl-tel-input-mask__custom/js/settings-mask.js"></script> -- 3 подключить иницилизацию settings-mask.js
+// <link rel="stylesheet" href="intl-tel-input-mask__custom/css/intlTelInput.css"> -- 4 подключить стили intlTelInput.css
 
 // https://github.com/digitalBush/jquery.maskedinput -- ссылки для дополнительных настроек масок
 // https://github.com/jackocnr/intl-tel-input.git -- ссылки для дополнительных настроек масок
